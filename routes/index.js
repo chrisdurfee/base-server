5cc45000068f7a590187039d4eb6b21058e14eef
tech_e <chris.durfee@dentalqore.com>
2024-09-13 00:59:37 -0600

    Update dependencies for @base-framework/atoms and @base-framework/base

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-import { Body, Head, Html, Title } from '@base-framework/atoms';
+import { Body, Div, Head, Html, Script } from '@base-framework/atoms';
 import { Builder } from '@base-framework/base';
 import express from 'express';
 
@@ -6,21 +6,23 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 });
 
 router.get('/test', function(req, res, next) {
-  const layout = () => (
-    Html([
-      Head([
-        Title('Test Page')
-      ]),
-      Body('This is a test page')
-    ])
-  );
+	const layout = () => (
+		Html([
+			Head([
+				Script()
+			]),
+			Body({ class: 'test-page' }, [
+				Div({ class: 'content' }, 'Hello World!')
+			])
+		])
+	);
 
-  const html = Builder.render(layout);
-  res.send(html);
+	const html = Builder.render(layout);
+	res.send(html);
 });
 
 export default router;
