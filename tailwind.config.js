a18376528b555d0357ae781f44335a40f12dfb58
tech_e <chris.durfee@dentalqore.com>
2024-09-30 01:28:30 -0600

    Add Tailwind CSS configuration and stylesheets

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./routes/**/*.{js,ts,jsx,tsx}",
+    "./routes/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
