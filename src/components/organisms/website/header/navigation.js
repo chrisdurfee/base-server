797ec423fe7e4de0a47d585ac4f0e8e73a8fdda9
tech_e <chris.durfee@dentalqore.com>
2024-09-30 20:47:22 -0600

    Refactor navigation component and clean up main CSS styles

--- src/components/organisms/website/header/navigation.js
+++ src/components/organisms/website/header/navigation.js
@@ -28,13 +28,16 @@ const Logo = Atom(({ src = 'https://tailwindui.com/img/logos/mark.svg?color=indi
 const MobileMenuButton = Atom((props) =>
 {
     const className = '-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700';
-    return Button({ type: 'button', class: className }, [
-        Span({ class: 'sr-only' }, 'Open main menu'),
-        Div({
-            html: `<svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-            </svg>`
-        })
+
+    return Div({ class: 'flex lg:hidden' }, [
+        Button({ type: 'button', class: className }, [
+            Span({ class: 'sr-only' }, 'Open main menu'),
+            Div({
+                html: `<svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>`
+            })
+        ])
     ]);
 });
 
@@ -53,8 +56,8 @@ const DesktopMenuLinks = Atom(({ links = ['Product', 'Features', 'Marketplace', 
 {
     return Div({
         class: 'hidden lg:flex lg:gap-x-12' ,
-        map: [links, Link]
-    });
+        //map: [links, Link]
+    }, links.map(Link));
 });
 
 /**
@@ -66,7 +69,7 @@ const DesktopLogIn = Atom(() =>
 {
     return Div({ class: 'hidden lg:flex lg:flex-1 lg:justify-end' }, [
         A({ href: '#', class: 'text-sm font-semibold leading-6 text-gray-900' }, [
-            'Log in ',
+            Span('Log in '),
             Span({ 'aria-hidden': 'true' }, '→')
         ])
     ]);
